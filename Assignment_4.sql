232559cb94de9250c82407cbddd64b465fdf379c
hritikahirwar456 <107921927+hritikahirwar456@users.noreply.github.com>
2023-06-30 22:27:22 +0530

    Update Assignment_4.sql

--- Assignment_4.sql
+++ Assignment_4.sql
@@ -1,35 +1,30 @@
-CREATE DATABASE GITHUB;
-SHOW DATABASES;
-USE GITHUB;
-
--- Existing Market Size
+CREATE DATABASE GITHUB_PM;
+USE GITHUB_PM;
 CREATE TABLE existing_market_size (
   year INT,
-  individual_developers INT,
-  enterprise_customers INT,
-  smes INT,
-  education_research INT,
-  open_source_communities INT
+  developers_in_Millions INT,
+  repositories_created_in_Millions INT,
+  active_organizations_in_Millions INT
 );
 
-INSERT INTO existing_market_size (year, individual_developers, enterprise_customers, smes, education_research, open_source_communities)
+INSERT INTO existing_market_size (year, developers_in_Millions, repositories_created_in_Millions, active_organizations_in_Millions)
 VALUES
-  (2018, 5000000, 2000, 10000, 500000, 1000000),
-  (2019, 6000000, 2500, 12000, 550000, 1200000),
-  (2020, 7000000, 3000, 15000, 600000, 1500000),
-  (2021, 8000000, 3500, 18000, 650000, 1800000);
-
--- Proposed Market Size
+  (2016, 5.8, 19.4, 331000),
+  (2017, 24, 67, 1500000),
+  (2018, 31, 96, 2100000),
+  (2019, 40, 44, 2900000),
+  (2020, 56, 60, NULL),
+  (2022, 94, NULL, NULL);
 CREATE TABLE proposed_market_size (
   year INT,
-  individual_developers INT,
-  enterprise_customers INT,
-  smes INT,
-  education_research INT,
-  open_source_communities INT
+  developers_in_Millions INT,
+  repositories_created_in_Millions INT,
+  active_organizations_in_Millions INT
 );
 
-INSERT INTO proposed_market_size (year, individual_developers, enterprise_customers, smes, education_research, open_source_communities)
+INSERT INTO proposed_market_size (year, developers_in_Millions, repositories_created_in_Millions, active_organizations_in_Millions)
 VALUES
-  (2022, 9000000, 4000, 20000, 700000, 2000000),
-  (2023, 10000000, 4500, 22000, 750000, 2200000);
+  (2022, 94, 200, NULL),
+  (2023, NULL, NULL, NULL);
+  SELECT * FROM existing_market_size;
+  SELECT * FROM proposed_market_size;
